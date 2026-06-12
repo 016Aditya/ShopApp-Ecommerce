9b724340e75ada00544dd7a362a62f2c0b8dd090
016Aditya <016adityasingh@gmail.com>
2026-06-13 01:13:31 +0530

    Remove unused imports and variables in PageWrapper.jsx

--- src/components/layout/PageWrapper.jsx
+++ src/components/layout/PageWrapper.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
-import CategoryBar from "./CategoryBar";
 import Footer from "./Footer";
 
 const PageWrapper = () => {
   return (
     <>
       <Navbar />
-      <CategoryBar />
       <main>
         <Outlet />
       </main>
